50f63700490ed8c03259a80da49cc6b423e1b6da
mshr-h <mshr-h@users.noreply.github.com>
2016-09-21 15:19:55 +0900

    add test differential copy

--- test.sh
+++ test.sh
@@ -4,25 +4,33 @@ rm -rf testdir/
 # prepare testdir
 mkdir -p testdir/source/directory1
 mkdir -p testdir/source/directory2
-touch    testdir/source/directory1/file1
-touch    testdir/source/directory1/file2
-touch    testdir/source/directory2/file3
-touch    testdir/source/directory2/file4
-touch    testdir/source/file5
-touch    testdir/source/file6
+echo     "This is file1." > testdir/source/directory1/file1
+echo     "This is file2." > testdir/source/directory1/file2
+echo     "This is file3." > testdir/source/directory2/file3
+echo     "This is file4." > testdir/source/directory2/file4
+echo     "This is file5." > testdir/source/file5
+echo     "This is file6." > testdir/source/file6
 
 mkdir -p testdir/destination/dummydirectory
-touch    testdir/destination/dummydirectory/dummyfile1
-touch    testdir/destination/dummyfile2
+echo     "This is dummyfile1." > testdir/destination/dummydirectory/dummyfile1
+echo     "This is dummyfile2." > testdir/destination/dummyfile2
 
 go run main.go testdir/source testdir/destination
 
-if [ -z "$(diff -r -q testdir/source testdir/destination)" ]; then
+if [ -n "$(diff -r -q testdir/source testdir/destination)" ]; then
+  echo "NG"
+  exit 1
+fi
+
+echo     "Append" >> testdir/source/directory1/file1
+go run main.go testdir/source testdir/destination
+
+if [ -n "$(diff -r -q testdir/source testdir/destination)" ]; then
+  echo "NG"
+  exit 1
+else
   echo "OK"
   rm -rf testdir
   exit 0
-else
-  echo "NG"
-  exit 1
 fi
 
